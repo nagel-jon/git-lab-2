7a8a009e84b16587a3508b7e0e5420fdc306a804
nagel-jon <jn855420@ohio.edu>
2021-09-02 14:30:46 -0400

    second commit

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Jonathan Nagel
+ *        Date: 9/2/21
+ * Description: Copied program for git-lab
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+cout << "Hello Git!" << endl;
+    /*add code*/
+    return 0;
+}// main
